a0a714e4be06c311327235ea4ef5100f1fa28b34
Viraj Kaushalye <virajkaush@gmail.com>
2018-09-19 07:27:38 +0530

    Add Project v2.0

---
+++ Assets/Scripts/NavPrefabController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavPrefabController : MonoBehaviour
+{
+	public Color color;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
